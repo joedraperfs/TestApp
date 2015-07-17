37862a3f5df14aebecc98c6529718279030fc567
Joe Draper <joe.draper@gmail.com>
2015-07-16 21:34:31 -0400

    Added line to indicate Experiment 1 Git branch.

--- code/com/company/testapp/TestAppMain.java
+++ code/com/company/testapp/TestAppMain.java
@@ -5,5 +5,6 @@ public class TestAppMain
 	public static void main(String[] args)
 	{
 		System.out.println( "Hello from TestAppMain!" );
+		System.out.println( "Code from Git Branch Experiment1!" );
 	}
 }
